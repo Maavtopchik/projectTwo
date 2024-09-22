b6343838fe2f3fabd46534173a6fd87468bf2e80
Maavtopchik <vdf12354g@gmail.com>
2024-09-22 12:37:40 +0300

    first

---
+++ j.js
@@ -0,0 +1,50 @@
+
+let myclass = document.getElementById('box1');
+
+let p = 0;
+function pref(){
+
+if(p == 0){
+    myclass.style.display = 'block';
+    p = 1
+}else if(p==1){
+    myclass.style.display = 'none';
+    p = 0
+}
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
